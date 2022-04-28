7fc69f3b2abe8b5aea6ef39cccaf163071612f1c
coke5151 <houjunqimail@gmail.com>
2022-04-28 09:59:14 +0800

    0428自主學習

---
+++ main.js
@@ -0,0 +1,11 @@
+function homepg() {
+    this.top.location.href = "/index.html";
+}
+
+function diary() {
+    this.top.location.href = "/diary.html";
+}
+
+function about() {
+    this.top.location.href = "/about.html";
+}
